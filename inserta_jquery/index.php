8d9fe97c1b9519ae3251e46200b17cbd70e8040e
BeikSant <beiksantorum@unl.edu.ec>
2021-02-25 16:21:50 -0500

    Practicas en Clase con JQuery

--- inserta_jquery/index.php
+++ inserta_jquery/index.php
@@ -4,50 +4,86 @@
     <meta charset="utf-8">
     <title>insertar datos con ajax</title>
     <script src="jquery-3.2.1.min.js"></script>
+    <!-- CSS only -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	
 </head>
 <body>
 
-    <form id="frmajax" method="POST">
-        <label>Nombre</label>
-        <p></p>
-        <input type="text" name="nombre" id="nombre">
-        <p></p>
-        <label>apellido</label>
-        <p></p>
-        <input type="text" name="apellido" id="apellido">
-        <p></p>
-        <label>Usuario</label>
-        <p></p>
-        <input type="text" name="usuario" id="usuario">
-        <p></p>
-        <label>password</label>
-        <p></p>
-        <input type="text" name="password" id="password">
-        <p></p>
-        <button id="btnguardar">Guardar datos</button>
-    </form>
+<!DOCTYPE html>
+<html>
 
-</body>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <a class="navbar-brand">UNIVERSIDAD NACIONAL DE LOJA</a>
+                
+                <ul class="navbar-nav ml-auto">
+                    <form class="form-inline my-2 my-lg-0">
+                        <input name="search" id="search" class="form-control mr-sm-2" type="search"
+                            placeholder="Buscar Task" aria-label="Search">
+                        <button class="btn btn-success my-2 my-sm-0" type="submit">Buscar</button>
+                    </form>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="jumbotron">
+            <form id="frmajax" method="POST">
+                <div class="mb-3">
+                    <label for="nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" name="nombre" id="nombre">
+                </div>
+                <div class="mb-3">
+                    <label for="apellido" class="form-label">Apellido</label>
+                    <input type="text" class="form-control" name="apellido" id="apellido">
+                </div>
+                <div class="mb-3">
+                    <label for="usuario" class="form-label">Usuairo</label>
+                    <input type="text" class="form-control" name="usuario" id="usuario">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password </label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <button class="btn btn-primary" id="btnguardar">Guardar datos</button>
+            </form>
+        </div>
+
+        <div class="card-footer text-light bg-dark">
+            <footer>
+                <center>
+                    &copy;All rights reserved. |<br>
+                    Estudiante: Beiker Santorum<br>
+                    Curso: 5º "A<br>
+                    Teléfono: 0986005899<br>
+                    Cedula: 150031375
+                </center>
+            </footer>
+        </div>
+    </body>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#btnguardar').click(function(){
+                var datos = $('#frmajax').serialize();
+                $.ajax({
+                    type:"POST",
+                    url:"insertar.php",
+                    data:datos,
+                    success: function(respuesta){
+                        if(respuesta == 1){
+                            alert("Agregado con éxito");
+                        } else {
+                            alert("Fallo el servidor");
+                        }
+                    }
+                });
+                return false;
+            });
+        });
+    </script>
 </html>
 
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#btnguardar').click(function(){
-            var datos = $('#frmajax').serialize();
-            $.ajax({
-                type:"POST",
-                url:"insertar.php",
-                data:datos,
-                success: function(respuesta){
-                    if(respuesta == 1){
-                        alert("Agregado con éxito");
-                    } else {
-                        alert("Fallo el servidor");
-                    }
-                }
-            });
-            return false;
-        });
-    });
-</script>
 
---
+++ inserta_jquery/insertar.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Linux (x86_64)
+--
+-- Host: localhost    Database: insertar
+-- ------------------------------------------------------
+-- Server version	8.0.23-0ubuntu0.20.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `apellido` varchar(50) DEFAULT NULL,
+  `usuario` varchar(50) DEFAULT NULL,
+  `password` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (15,'','','',''),(16,'','','',''),(17,'','','',''),(18,'','','',''),(19,'aaaa','aaaaa','aaaa','aaaaa'),(20,'Jose','Santorum','beiksant','122344'),(21,'Beiker','Santorum','beiker','454444'),(22,'Mario','Cuenva','mario','45222'),(23,'Maria','Rosales','maria','444444'),(24,'Maria','Rosales','maria','444444'),(25,'Maria','Rosales','maria','444444'),(26,'Rosa','Armijos','rosa','444444');
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-02-25 16:21:01
